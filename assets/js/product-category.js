4789bbe8a145f74e86ac1f48c927f8522e0124ab
unknown <phamvannam.dev@gmail.com.com>
2024-10-10 23:45:17 +0700

    fe/responsive product-category

--- assets/js/product-category.js
+++ assets/js/product-category.js
@@ -1,5 +1,6 @@
 function toggleColors() {
     $('.filter-checkbox .form-check[data-visible="false"]').each(function () {
+        // Lặp qua các phần tử và thay đổi hiển thị của chúng
         $(this).toggle();
     });
 
@@ -13,5 +14,44 @@ function toggleColors() {
 }
 
 $(document).ready(function () {
-    $('#toggleColors').on('click', toggleColors);
+    $('.toggleColors').on('click', toggleColors);
+});
+
+$(document).ready(function () {
+    $('.viewed_products-list').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: false,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+        ],
+    });
 });
