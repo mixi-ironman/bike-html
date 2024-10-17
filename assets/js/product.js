d112f6dfaeb74f2319d3b75748517b270998b096
unknown <phamvannam.dev@gmail.com.com>
2024-10-17 14:31:52 +0700

    .

--- assets/js/product.js
+++ assets/js/product.js
@@ -135,6 +135,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         dots: false,
+        // asNavFor: '.product-detail_wrap-thumbnail', // Liên kết với thumbnail slider
         prevArrow:
             "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
         nextArrow:
@@ -166,16 +167,88 @@ $(document).ready(function () {
             },
         ],
     });
-});
 
-$(document).ready(function () {
-    function changeImage(targetElement, event, mainImageClass) {
-        event.preventDefault();
-        var url = $(targetElement).data('img');
-        $(mainImageClass).attr('src', url);
-    }
-
-    $(document).on('click', '.product-detail_img-thumbnail', function (event) {
-        changeImage(this, event, '.mainImage');
+    // Khởi tạo slider cho thumbnail
+    $('.product-detail_wrap-thumbnail').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        vertical: true, // Chuyển sang chế độ theo chiều dọc
+        draggable: false, // Tắt khả năng kéo (drag)
+        infinite: false,
     });
+
+    // Xử lý để làm thumbnail "active" khi ảnh chính thay đổi
+    $('.product-detail_wrap-img_main-list').on('afterChange', function (event, slick, currentSlide) {
+        // Xóa class active khỏi tất cả thumbnail
+        $('.product-detail_wrap-thumbnail .product-detail_img-thumbnail').removeClass('active');
+
+        // Thêm class active cho thumbnail tương ứng
+        $(
+            '.product-detail_wrap-thumbnail .product-detail_img-thumbnail[data-slick-index="' + currentSlide + '"]',
+        ).addClass('active');
+    });
+
+    // Khi click vào thumbnail, làm ảnh chính thay đổi
+    $('.product-detail_wrap-thumbnail .product-detail_img-thumbnail').on('click', function () {
+        var index = $(this).data('slick-index');
+        $('.product-detail_wrap-img_main-list').slick('slickGoTo', index); // Chuyển đến ảnh tương ứng
+    });
+
+    // -------------------------------------------------------------------
+
+    // Kiểm tra và ẩn/hiện các nút mũi tên khi slider thay đổi
+    $('.product-detail_wrap-img_main-list,.similar_products-list,.same_products-list,.viewed_products-list').on(
+        'afterChange',
+        function (event, slick, currentSlide) {
+            var totalSlides = slick.slideCount;
+            console.log(currentSlide);
+
+            // Ẩn nút prevArrow nếu đang ở item đầu tiên
+            if (currentSlide === 0) {
+                $('.slick-prev').hide();
+                // $('.slick-prev').prop('disabled', true);
+            } else {
+                $('.slick-prev').show();
+            }
+
+            // Ẩn nút nextArrow nếu đang ở item cuối cùng
+            if (currentSlide >= totalSlides - slick.options.slidesToShow) {
+                $('.slick-next').hide();
+            } else {
+                $('.slick-next').show();
+            }
+        },
+    );
+
+    // Kiểm tra lần đầu khi slider được khởi tạo
+    $('.product-detail_wrap-img_main-list,.similar_products-list,.same_products-list,.viewed_products-list').on(
+        'init',
+        function (event, slick) {
+            var totalSlides = slick.slideCount;
+
+            // Ẩn nút prevArrow nếu slider bắt đầu từ item đầu tiên
+            if (slick.currentSlide === 0) {
+                $('.slick-prev').hide();
+            }
+
+            // Ẩn nút nextArrow nếu không đủ item để cuộn
+            if (slick.currentSlide >= totalSlides - slick.options.slidesToShow) {
+                $('.slick-next').hide();
+            }
+        },
+    );
 });
+
+// $(document).ready(function () {
+//     function changeImage(targetElement, event, mainImageClass) {
+//         event.preventDefault();
+//         var url = $(targetElement).data('img');
+//         $(mainImageClass).attr('src', url);
+//     }
+
+//     $(document).on('click', '.product-detail_img-thumbnail', function (event) {
+//         changeImage(this, event, '.mainImage');
+//     });
+// });
